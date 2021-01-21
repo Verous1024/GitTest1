9d3c2bffc91e71f6c55db374835c6eb64146702f
tom_glb <1987313921@qq.com>
2021-01-21 20:20:33 +0800

    采纳C的建议

---
+++ src/programmerC.java
@@ -0,0 +1,11 @@
+/**
+ * Descriptions:
+ *
+ * @author Verous
+ * @version 1.0 2021-01-21 下午 08:19
+ */
+public class programmerC {
+    public static void main(String[] args) {
+        System.out.println("程序员 提出的建议并实现的代码");
+    }
+}
