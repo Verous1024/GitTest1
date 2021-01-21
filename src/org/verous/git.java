1b0dc402562984abfaa0e8457f3e5498a006e512
tom_glb <1987313921@qq.com>
2021-01-21 11:47:20 +0800

    Initial commit

---
+++ src/org/verous/git.java
@@ -0,0 +1,13 @@
+package org.verous;
+
+/**
+ * Descriptions:
+ *
+ * @author Verous
+ * @version 1.0 2021-01-21 上午 10:50
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
