f3b198081a2000115b484f4df817ebce2ddd98e2
tom_glb <1987313921@qq.com>
2021-01-21 15:31:56 +0800

    新文件NewMain

---
+++ src/NewMain.java
@@ -0,0 +1,11 @@
+/**
+ * Descriptions:
+ *
+ * @author Verous
+ * @version 1.0 2021-01-21 下午 03:29
+ */
+public class NewMain {
+    public static void main(String[] args) {
+        System.out.println("新文件");
+    }
+}
