01d2f6499dcddaef80417f467183dc0f4ea96188
tom_glb <1987313921@qq.com>
2021-01-21 20:17:51 +0800

    再采纳A的建议

---
+++ src/programmerA.java
@@ -0,0 +1,11 @@
+/**
+ * Descriptions:
+ *
+ * @author Verous
+ * @version 1.0 2021-01-21 下午 08:16
+ */
+public class programmerA {
+    public static void main(String[] args) {
+        System.out.println("程序员A提出的建议并实现的代码");
+    }
+}
